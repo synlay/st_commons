989a3f0360a7debb806a79ce2146275c2dd4f794
David Robakowski <david.robakowski@synlay.com>
2014-01-24 13:52:08 +0100

    Added method throw_ifneq/2

--- src/libmisc.erl
+++ src/libmisc.erl
@@ -13,6 +13,7 @@
 	,get_optional/3
 	,get_required/2
     ,epoch_timestamp/0
+    ,throw_ifneq/2
 ]).
 
 -include("libmisc.hrl").
@@ -76,3 +77,17 @@ get_required(AppEnv, Key) ->
     EpochTimestamp :: epoch_timestamp().
 epoch_timestamp() ->
     calendar:datetime_to_gregorian_seconds(calendar:universal_time()) - 62167219200.
+
+%% @doc Throws an exception with the return value of Fun if it doesn't equals
+%%      Pattern, otherwise Pattern will be returned.
+%%      GuardExpr must be any value that can be be used for pattern matching.
+-spec throw_ifneq(Fun, Pattern) -> Pattern | none() when
+    Fun     :: fun(() -> term()),
+    Pattern :: term().
+throw_ifneq(Fun, Pattern) ->
+    case Fun() of
+        Pattern ->
+            Pattern;
+        Other ->
+            throw(Other)
+    end.
