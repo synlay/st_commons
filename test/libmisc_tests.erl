f928bd61cde2cc18cd5ff625444eb8b2a4288bd6
David Robakowski <david.robakowski@synlay.com>
2015-03-21 14:31:56 +0100

    Add temporary test stub

--- test/libmisc_tests.erl
+++ test/libmisc_tests.erl
@@ -1,1 +1,19 @@
 -module(libmisc_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+libmisc_test_() ->
+    {setup,
+        fun() ->
+            ok
+        end,
+        fun(_) ->
+            ok
+        end,
+        [
+            {"libmisc_tests is alive",
+                fun() ->
+                    %% format is always: expected, actual
+                    ?assert(true)
+                end}
+        ]}.
